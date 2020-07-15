6c60796121732cbafa46478ace9461cc5e1aeb4a
Conner Charlebois <cdcharlebois@gmail.com>
2020-07-15 16:26:28 -0400

    release

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -1,55 +1,45 @@
 import { Component, createElement } from "react";
-import { View, Text } from "react-native";
+import { Text, View } from "react-native";
 
 import Lib from "react-native-collapsible/Accordion";
 
-const styles = {
-  header: {
-    padding: 5
-  },
-  headerText: {
-    textAlign: "center"
-  }
-}
-
 export class Accordion extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeSections: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeSections: []
+        };
+        this._updateSections = this._updateSections.bind(this);
+        this._renderHeader = this._renderHeader.bind(this);
     }
-    this._updateSections = this._updateSections.bind(this);
-    this._renderHeader = this._renderHeader.bind(this);
-  }
 
-  _updateSections(activeSections) {
-    this.setState({
-      activeSections: activeSections
-    });
-  }
+    _updateSections(activeSections) {
+        this.setState({
+            activeSections: activeSections
+        });
+    }
 
-  _renderHeader(item, index, isActive) {
-    const { headerOpen, headerClosed } = this.props;
-    return isActive ? headerOpen(item) : headerClosed(item)
-  }
+    _renderHeader(item, index, isActive) {
+        const { headerOpen, headerClosed } = this.props;
+        return isActive ? headerOpen(item) : headerClosed(item);
+    }
 
-  render() {
-    const { ds, body } = this.props;
-    return (
-      ds.status === 'available' ?
-        <Lib
-          sections={ds.items}
-          activeSections={this.state.activeSections}
-          renderHeader={this._renderHeader}
-          renderContent={body}
-          onChange={this._updateSections}
-          underlayColor={"transparent"}
-          expandMultiple={true}
-        />
-        :
-        <View>
-          <Text>Loading...</Text>
-        </View>
-    );
-  }
+    render() {
+        const { ds, body } = this.props;
+        return ds.status === "available" ? (
+            <Lib
+                sections={ds.items}
+                activeSections={this.state.activeSections}
+                renderHeader={this._renderHeader}
+                renderContent={body}
+                onChange={this._updateSections}
+                underlayColor={"transparent"}
+                expandMultiple={true}
+            />
+        ) : (
+            <View>
+                <Text>Loading...</Text>
+            </View>
+        );
+    }
 }
